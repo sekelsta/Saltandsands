c7e63599b8162fccdbf7defd5eb59ea9d68c46bf
Catasteroid <howescaleb@gmail.com>
2023-12-03 22:47:22 +0000

    Update SaltandsandCoral.cs

--- src/SaltandsandCoral.cs
+++ src/SaltandsandCoral.cs
@@ -51,76 +51,16 @@ namespace Saltandsands
         // Worldgen placement, tests to see how many blocks below water the plant is being placed, and if that's allowed for the plant
         public override bool TryPlaceBlockForWorldGen(IBlockAccessor blockAccessor, BlockPos pos, BlockFacing onBlockFace, LCGRandom worldGenRand)
         {
-            //Block block = blockAccessor.GetBlock(pos);
-
-            //if (!block.IsReplacableBy(this))
-            //{
-            //    return false;
-            //}
 
             Block aboveBlock = blockAccessor.GetBlock(pos.X, pos.Y + 1, pos.Z);
-
-			/*
-            if (belowBlock.Fertility > 0 && minDepth == 0)
-            {
-                Block placingBlock = blockAccessor.GetBlock(Code);
-                if (placingBlock == null) return false;
-                
-                blockAccessor.SetBlock(placingBlock.BlockId, pos);
-                return true;
-            }
-			*/
-			
-            if(aboveBlock.LiquidCode == waterCode)
-            {
 
 				int rnd = worldGenRand.NextInt(coralTypes.Length-1);
 				Api.World.Logger.Error("Selected coral type {0}",coralTypes[rnd]);				
 				Block coralPlacingBlock = blockAccessor.GetBlock(coralTypes[rnd]);
 				Api.World.Logger.Error("Coral block resolved: {0}, block ID: {1}",coralPlacingBlock.Code,coralPlacingBlock.Id);
-				blockAccessor.SetBlock(coralPlacingBlock.Id, pos.Up());
+				blockAccessor.SetBlock(coralPlacingBlock.Id, pos.UpCopy());
 				Api.World.Logger.Error("Placed coral substrate at depth {0}, coral type: {1}, coral code: {2}!",currentDepth,coralStrings[rnd],coralTypes[rnd]);
 				return true;
-				/*
-                for(var currentDepth = 1; currentDepth <= maxDepth + 1; currentDepth ++)
-                {
-                    aboveBlock = blockAccessor.GetBlock(pos.X, pos.Y + currentDepth, pos.Z);
-					if(currentDepth < minDepth + 1) return false;
-					//if(aboveBlock.LiquidCode != waterCode) return false;
-				}
-                    //if (aboveBlock.Fertility > 0)
-                    //{
-						Block aboveBlock = blockAccessor.GetBlock(pos.X, pos.Y - currentDepth + 1, pos.Z);
-                        if(aboveBlock.LiquidCode != waterCode) return false;
-						if(currentDepth < minDepth + 1) return false;
-						Api.World.Logger.Notification("Attempting to place coral substrate on lake/seabed, watercode: {0}, depth: {1}",waterCode,currentDepth);
-
-						if (blockAccessor.GetBlock(pos).Replaceable > 500)
-						{
-							blockAccessor.SetBlock(placingBlock.BlockId, pos.DownCopy(currentDepth - 1));
-						
-							int rnd = worldGenRand.NextInt(coralTypes.Length-1);
-							
-							Block coralPlacingBlock = blockAccessor.GetBlock(coralTypes[rnd]);
-							blockAccessor.SetBlock(coralPlacingBlock.Id, pos.UpCopy(currentDepth));
-							Api.World.Logger.Error("Placed coral substrate at depth {0}, coral type: {1}, coral code: {2}!",currentDepth,coralStrings[rnd],coralTypes[rnd]);
-							//Api.World.Logger.Notification($"Placed coral substrate and coral block successfully!, depth: {waterCode}, coral type: {coralStrings[rnd]}, coral code: {coralTypes[rnd]}"); 
-							return true;
-						}
-						else
-						{
-							Api.World.Logger.Notification("Could not place coral substrate at depth {0}!",currentDepth);
-						}
-                        return false;
-                    //}
-					*/
-                
-            }
-			else
-			{
-				Api.World.Logger.Error("Failed coral substrate placement!");
-			}
-            return false;
         }   
     }
 	
