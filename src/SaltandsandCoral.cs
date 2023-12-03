2b396030044edb211e625773833039cfe9e0dd34
Catasteroid <howescaleb@gmail.com>
2023-12-03 22:11:16 +0000

    fixed extra bracket

--- src/SaltandsandCoral.cs
+++ src/SaltandsandCoral.cs
@@ -111,7 +111,7 @@ namespace Saltandsands
                         return false;
                     //}
 					*/
-                }
+                
             }
             return false;
         }   
